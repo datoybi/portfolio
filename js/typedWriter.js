b6efaf8357951800ada9acbad4f026a0c9be658c
dasomyun <dsy0302@gmail.com>
2019-01-10 13:08:40 +0900

    making typing effect

---
+++ js/typedWriter.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#typed', {
+    strings: ["fullstack developer", "web developer", "front-end developer"],             
+    typeSpeed: 50,
+    loop: true,
+    backDelay: 2000,
+    backSpeed: 25
+});
