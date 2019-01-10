81fedf52146463dfea6e81c49a4571db56101d23
dasomyun <dsy0302@gmail.com>
2019-01-10 14:13:59 +0900

    update no drag effect, background.

--- js/typedWriter.js
+++ js/typedWriter.js
@@ -1,5 +1,5 @@
 var typed = new Typed('#typed', {
-    strings: ["fullstack developer", "web developer", "front-end developer"],             
+    strings: ["fullstack developer.", "web developer.", "front-end developer."],             
     typeSpeed: 50,
     loop: true,
     backDelay: 2000,
