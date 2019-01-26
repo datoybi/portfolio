32fe918ad0e59677d962a041f2507eb50b1c1d7f
dasomyun <dsy0302@gmail.com>
2019-01-26 16:52:04 -0500

    update navbar(not finished)

--- js/index.js
+++ js/index.js
@@ -31,3 +31,13 @@ $(document).ready(function() {
         } 
       });
  });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
