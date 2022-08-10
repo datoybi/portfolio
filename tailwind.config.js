82c33db46017613253626e1e9870780140ae6b31
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2022-08-10 23:52:00 +0900

    tailwind 초기세팅중

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// https://tailwindcss.com/docs/content-configuration
