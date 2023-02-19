be1981c7813e86066fc78153d83757f76d4b61be
윤다솜 (Dasom Yun) <dsy0302@gmail.com>
2023-02-19 22:51:22 +0900

    :memo: docs: 포폴 추가

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ export function Title({ $app, initialState }) {
     $app.innerHTML = `
 		<article>
 			<section class="pt-16">
-				<h1 class="text-6xl font-bold inline p-2 highlight leading-normal tracking-[-3px]">안녕하세요!<br>2년차 프론트앤드 개발자 ${this.state} 입니다.</h1>
+				<h1 class="text-6xl font-bold inline p-2 highlight leading-normal tracking-[-3px]">안녕하세요!<br>프론트앤드 개발자 ${this.state} 입니다.</h1>
 			</section>
 		</article>`;
   };
